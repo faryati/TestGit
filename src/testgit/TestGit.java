02d48bb1ff88802cc09a9b6be8296bd9609d0b8e
fatima EL FARYATI <fati.faryati@gmail.com>
2016-12-02 11:47:52 +0000

    version 1 , fuction add

---
+++ src/testgit/TestGit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author Fatima EL FARYATI
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    /*public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour");
+    }*/
+    public int add(int a, int b){
+        return a+b;
+    }
+}
